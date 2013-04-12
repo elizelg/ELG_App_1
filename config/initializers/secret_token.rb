549dc8ba33b6ec4cfdfd55d5cc20222496179bb7
Elize la Grange <elize.lagrange@gmail.com>
2013-04-12 11:53:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElizeDemoApp::Application.config.secret_token = 'ca96c1d6ef45a0ab65a62e89e2f388c7b39eb051a2e2e6917fd2c0273bb2550a38267eaaa7d3c3355466bb0ecd8b84976e52d809074396e9ac9638a187b12a5b'
